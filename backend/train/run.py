8962a4ac833ce18efa8e157e2bd97e42bca0c5ce
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-05-11 16:41:35 +0800

    fake process for Flower server

--- backend/train/run.py
+++ backend/train/run.py
@@ -1,17 +1,30 @@
 from logging import getLogger
 from multiprocessing.connection import Connection
+from time import sleep
+
+PORT = 8080
 
 logger = getLogger(__name__)
 
 
 def execute(conn: Connection):
-    msg = conn.recv()
-    # TODO: Use the message.
-    logger.warning(f"Received `{msg}`.")
-    conn.send(msg)
+    """Execute one instruction received."""
+    kind, msg = conn.recv()
+    if kind == "ping":
+        logger.warning(f"Received `{msg}`.")
+        conn.send("pong")
+    elif kind == "server":
+        logger.warning(f"Launching server with arguments `{msg}`.")
+        # TODO: Use `msg` and create a server.
+        sleep(10)
+        conn.send("done")
+    else:
+        logger.error(f"Unknown kind `{kind}` with message `{msg}`.")
+        conn.send("cannot understand")
 
 
 def run(conn: Connection):
+    """Run a procedure that listens to instructions and executes them."""
     logger.warning("Runner started.")
     while True:
         try:
--- backend/train/scheduler.py
+++ backend/train/scheduler.py
@@ -1,18 +1,94 @@
+from logging import getLogger
 from multiprocessing import Pipe, Process
+from multiprocessing.connection import Connection
+from time import sleep
 
-from train.run import run
+from train.models import TFLiteModel
+from train.run import PORT, run
 
-_conn, conn1 = Pipe()
-process = Process(target=run, args=(conn1,))
-started = False
+logger = getLogger(__name__)
+_conn: Connection | None = None
+process: Process
+running: TFLiteModel | None = None
 
 
-def conn():
-    global started
-    if not started:
-        process.start()
-        started = True
+def spawn():
+    """Spawn a new background process and assign `conn`."""
+    global _conn, process
+    _conn, conn1 = Pipe()
+    process = Process(target=run, args=(conn1,))
+    process.start()
     return _conn
 
 
-__all__ = ["conn"]
+def conn():
+    """Get the connection to the background process."""
+    if _conn is None:
+        return spawn()
+    return _conn
+
+
+def clear():
+    """Clear the messages received from the background process."""
+    while conn().poll():
+        received = conn().recv()
+        logger.warning(f"Clearing message `{received}`.")
+
+
+def ping():
+    """Ping the background process to check if it is available."""
+    conn().send(("ping", "ping"))
+
+
+def check_alive():
+    """Check if the background process is alive by pinging it and waiting for
+    response for at most 1ms."""
+    clear()
+    ping()
+    for _ in range(1000):
+        if conn().poll():
+            conn().recv()
+            return True
+        sleep(1e-6)
+    return False
+
+
+def ensure_alive():
+    """Check if the background process is alive. Spawn a new one if not."""
+    global process, _conn
+    if not check_alive():
+        process.kill()
+        spawn()
+
+
+def finish_unfinished():
+    """Update `running` on whether the background process has finished."""
+    global running
+    while running and conn().poll():
+        msg = conn().recv()
+        if msg == "done":
+            running = None
+            break
+        else:
+            logger.error("Unknown message `{msg}` from runner.")
+
+
+def server(model: TFLiteModel) -> tuple[str, int | None]:
+    """Request a Flower server. Return `(status, port)`.
+    `status` is "started" if the server is already running,
+    "new" if newly started,
+    or "occupied" if the background process is unavailable."""
+    global running
+    finish_unfinished()
+    if running:
+        if running == model:
+            return "started", PORT
+        else:
+            return "occupied", None
+    else:
+        # Start new server.
+        ensure_alive()
+        # TODO: Use model.
+        conn().send(("server", ()))
+        running = model
+        return "new", PORT
