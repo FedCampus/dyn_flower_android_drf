b1eef6195e8cf9e650464c463cee6a2695eb21d6
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-05-24 18:25:30 +0800

    clean up FlowerServiceRunnable

--- client/app/src/main/java/flwr/android_client/train/FlowerServiceRunnable.kt
+++ client/app/src/main/java/flwr/android_client/train/FlowerServiceRunnable.kt
@@ -8,8 +8,7 @@ import java.nio.ByteBuffer
 import java.util.concurrent.CountDownLatch
 
 class FlowerServiceRunnable {
-    private var failed: Throwable? = null
-    private var requestObserver: StreamObserver<ClientMessage>? = null
+    private lateinit var requestObserver: StreamObserver<ClientMessage>
 
     @Throws(RuntimeException::class)
     fun run(asyncStub: FlowerServiceGrpc.FlowerServiceStub, activity: MainActivity) {
@@ -17,73 +16,74 @@ class FlowerServiceRunnable {
         requestObserver = asyncStub.join(
             object : StreamObserver<ServerMessage> {
                 override fun onNext(msg: ServerMessage) {
-                    handleMessage(msg, activity)
+                    try {
+                        handleMessage(msg, activity)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
                 }
 
                 override fun onError(t: Throwable) {
                     t.printStackTrace()
-                    failed = t
                     finishLatch.countDown()
                     Log.e(TAG, t.message!!)
                 }
 
                 override fun onCompleted() {
                     finishLatch.countDown()
-                    Log.e(TAG, "Done")
+                    Log.d(TAG, "Done")
                 }
             })
     }
 
+    @Throws
     private fun handleMessage(message: ServerMessage, activity: MainActivity) {
-        try {
-            val weights: Array<ByteBuffer>
-            var c: ClientMessage? = null
-            if (message.hasGetParametersIns()) {
-                Log.e(TAG, "Handling GetParameters")
-                activity.setResultText("Handling GetParameters message from the server.")
-                weights = activity.fc.weights
-                c = weightsAsProto(weights)
-            } else if (message.hasFitIns()) {
-                Log.e(TAG, "Handling FitIns")
-                activity.setResultText("Handling Fit request from the server.")
-                val layers = message.fitIns.parameters.tensorsList
-                val nLayers = layers.size
-                assert(nLayers.toLong() == activity.train.model.n_layers)
-                val epoch_config = message.fitIns.configMap.getOrDefault(
-                    "local_epochs",
-                    Scalar.newBuilder().setSint64(1).build()
-                )!!
-                val local_epochs = epoch_config.sint64.toInt()
-                val newWeights = arrayOfNulls<ByteBuffer>(nLayers)
-                for (i in 0 until nLayers) {
-                    val bytes = layers[i].toByteArray()
-                    newWeights[i] = ByteBuffer.wrap(bytes)
-                }
-                val outputs = activity.fc.fit(newWeights, local_epochs)
-                c = fitResAsProto(outputs.first, outputs.second)
-            } else if (message.hasEvaluateIns()) {
-                Log.d(TAG, "Handling EvaluateIns")
-                activity.setResultText("Handling Evaluate request from the server")
-                val layers = message.evaluateIns.parameters.tensorsList
-                val nLayers = layers.size
-                assert(nLayers.toLong() == activity.train.model.n_layers)
-                val newWeights = arrayOfNulls<ByteBuffer>(nLayers)
-                for (i in 0 until nLayers) {
-                    val bytes = layers[i].toByteArray()
-                    newWeights[i] = ByteBuffer.wrap(bytes)
-                }
-                val inference = activity.fc.evaluate(newWeights)
-                val loss = inference.first.first
-                val accuracy = inference.first.second
-                activity.setResultText("Test Accuracy after this round = $accuracy")
-                val test_size = inference.second
-                c = evaluateResAsProto(loss, test_size)
+        val weights: Array<ByteBuffer>
+        val clientMessage = if (message.hasGetParametersIns()) {
+            Log.d(TAG, "Handling GetParameters")
+            activity.setResultText("Handling GetParameters message from the server.")
+            weights = activity.fc.weights
+            weightsAsProto(weights)
+        } else if (message.hasFitIns()) {
+            Log.d(TAG, "Handling FitIns")
+            activity.setResultText("Handling Fit request from the server.")
+            val layers = message.fitIns.parameters.tensorsList
+            val nLayers = layers.size
+            assert(nLayers.toLong() == activity.train.model.n_layers)
+            val epoch_config = message.fitIns.configMap.getOrDefault(
+                "local_epochs",
+                Scalar.newBuilder().setSint64(1).build()
+            )!!
+            val local_epochs = epoch_config.sint64.toInt()
+            val newWeights = arrayOfNulls<ByteBuffer>(nLayers)
+            for (i in 0 until nLayers) {
+                val bytes = layers[i].toByteArray()
+                newWeights[i] = ByteBuffer.wrap(bytes)
             }
-            requestObserver!!.onNext(c)
-            activity.setResultText("Response sent to the server")
-        } catch (e: Exception) {
-            e.printStackTrace()
+            val outputs = activity.fc.fit(newWeights, local_epochs)
+            fitResAsProto(outputs.first, outputs.second)
+        } else if (message.hasEvaluateIns()) {
+            Log.d(TAG, "Handling EvaluateIns")
+            activity.setResultText("Handling Evaluate request from the server")
+            val layers = message.evaluateIns.parameters.tensorsList
+            val nLayers = layers.size
+            assert(nLayers.toLong() == activity.train.model.n_layers)
+            val newWeights = arrayOfNulls<ByteBuffer>(nLayers)
+            for (i in 0 until nLayers) {
+                val bytes = layers[i].toByteArray()
+                newWeights[i] = ByteBuffer.wrap(bytes)
+            }
+            val inference = activity.fc.evaluate(newWeights)
+            val loss = inference.first.first
+            val accuracy = inference.first.second
+            activity.setResultText("Test Accuracy after this round = $accuracy")
+            val test_size = inference.second
+            evaluateResAsProto(loss, test_size)
+        } else {
+            throw Error("Unreachable! Unknown client message")
         }
+        requestObserver.onNext(clientMessage)
+        activity.setResultText("Response sent to the server")
     }
 }
 
