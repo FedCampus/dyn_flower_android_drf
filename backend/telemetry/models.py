4dfb7c1d410043abf4dcafcf3068854035f9e166
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-06-07 17:34:07 +0800

    record TrainingSession

--- backend/telemetry/models.py
+++ backend/telemetry/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from train.models import TFLiteModel
 
-# Create your models here.
+
+class TrainingSession(models.Model):
+    tflite_model = models.ForeignKey(
+        TFLiteModel, on_delete=models.CASCADE, related_name="training_sessions"
+    )
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Training session {self.id} for {self.tflite_model} {self.start_time} - {self.end_time}>"  # type: ignore
