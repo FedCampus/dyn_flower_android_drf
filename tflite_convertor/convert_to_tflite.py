2aa946ebe0f2083c78a17914cfa47db126bcd616
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-06-08 16:32:55 +0800

    encapsulate convert_to_tflite into functions

--- tflite_convertor/convert_to_tflite.py
+++ tflite_convertor/convert_to_tflite.py
@@ -1,57 +1,72 @@
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.regularizers import l2
-from tfltransfer import bases
-from tfltransfer import heads
-from tfltransfer import optimizers
+from tfltransfer import bases, heads, optimizers
 from tfltransfer.tflite_transfer_converter import TFLiteTransferConverter
 
-"""Define the base model.
 
-To be compatible with TFLite Model Personalization, we need to define a
-base model and a head model. 
+def define_base_model():
+    """Define the base model.
 
-Here we are using an identity layer for base model, which just passes the 
-input as it is to the head model.
-"""
-base = tf.keras.Sequential(
-    [tf.keras.Input(shape=(32, 32, 3)), tf.keras.layers.Lambda(lambda x: x)]
-)
+    To be compatible with TFLite Model Personalization, we need to define a
+    base model and a head model.
 
-base.compile(loss="categorical_crossentropy", optimizer="sgd")
-base.save("identity_model", save_format="tf")
+    Here we are using an identity layer for base model, which just passes the
+    input as it is to the head model.
+    """
+    base = tf.keras.Sequential(
+        [tf.keras.Input(shape=(32, 32, 3)), tf.keras.layers.Lambda(lambda x: x)]
+    )
 
-"""Define the head model.
-
-This is the model architecture that we will train using Flower. 
-"""
-head = tf.keras.Sequential(
-    [
-        tf.keras.Input(shape=(32, 32, 3)),
-        tf.keras.layers.Conv2D(6, 5, activation="relu"),
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-        tf.keras.layers.Conv2D(16, 5, activation="relu"),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(units=120, activation="relu"),
-        tf.keras.layers.Dense(units=84, activation="relu"),
-        tf.keras.layers.Dense(units=10, activation="softmax"),
-    ]
-)
-
-head.compile(loss="categorical_crossentropy", optimizer="sgd")
+    base.compile(loss="categorical_crossentropy", optimizer="sgd")
+    base.save("identity_model", save_format="tf")
 
 
-"""Convert the model for TFLite.
+def define_head_model():
+    """Define the head model.
 
-Using 10 classes in CIFAR10, learning rate = 1e-3 and batch size = 32
+    This is the model architecture that we will train using Flower.
+    """
+    head = tf.keras.Sequential(
+        [
+            tf.keras.Input(shape=(32, 32, 3)),
+            tf.keras.layers.Conv2D(6, 5, activation="relu"),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Conv2D(16, 5, activation="relu"),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(units=120, activation="relu"),
+            tf.keras.layers.Dense(units=84, activation="relu"),
+            tf.keras.layers.Dense(units=10, activation="softmax"),
+        ]
+    )
 
-This will generate a directory called tflite_model with five tflite models.
-Copy them in your Android code under the assets/model directory.
-"""
+    head.compile(loss="categorical_crossentropy", optimizer="sgd")
+    return head
 
-base_path = bases.saved_model_base.SavedModelBase("identity_model")
-converter = TFLiteTransferConverter(
-    10, base_path, heads.KerasModelHead(head), optimizers.SGD(1e-3), train_batch_size=32
-)
 
-converter.convert_and_save("tflite_model")
+def convert_to_tflite(head):
+    """Convert the model for TFLite.
+
+    Using 10 classes in CIFAR10, learning rate = 1e-3 and batch size = 32
+
+    This will generate a directory called tflite_model with five tflite models.
+    Copy them in your Android code under the assets/model directory.
+    """
+    base_path = bases.saved_model_base.SavedModelBase("identity_model")
+    converter = TFLiteTransferConverter(
+        10,
+        base_path,
+        heads.KerasModelHead(head),
+        optimizers.SGD(1e-3),
+        train_batch_size=32,
+    )
+
+    converter.convert_and_save("tflite_model")
+
+
+def main():
+    define_base_model()
+    head = define_head_model()
+    convert_to_tflite(head)
+
+
+if __name__ == "__main__":
+    main()
