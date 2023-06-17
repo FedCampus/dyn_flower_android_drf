74cd4a25d7b804a7f33d39c747ab4ff7ba177991
Johnnybyzhang <sx100520zby@126.com>
2023-06-17 23:17:38 +0800

    fix path

--- backend/run.sh
+++ backend/run.sh
@@ -4,10 +4,10 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First startup, initializing database --"
-    cat seed.py | python3 manage.py shell
+    cat /app/seed.py | python3 /app/manage.py shell
 else
     echo "-- Not first container startup, skipping initialization --"
 fi
 # run the server
 echo "-- Starting development server --"
-python3 manage.py runserver 0.0.0.0:8000
+python3 /app/manage.py runserver 0.0.0.0:8000
