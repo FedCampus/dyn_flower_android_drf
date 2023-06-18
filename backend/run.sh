70d13233b9861cd305a8a1b55b9bb829f4d12481
Boyan Zhang <42496003+Johnnybyzhang@users.noreply.github.com>
2023-06-18 16:28:31 +0800

    Updated Docker Instructions, Improved Dockerfile, and Streamlined Container Initialization #19

---
+++ backend/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First startup, initializing database --"
+    cat /app/seed.py | python3 /app/manage.py shell
+else
+    echo "-- Not first container startup, skipping initialization --"
+fi
+# run the server
+echo "-- Starting development server --"
+python3 /app/manage.py runserver 0.0.0.0:8000
