76ff9f79132f4682e6961fb72ac2c539fc6af2b6
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-06-24 09:22:00 +0800

    clean up example_convert;pyproject.toml for poetry shell

--- tflite_convertor/example_convert.py
+++ tflite_convertor/example_convert.py
@@ -1,83 +1,114 @@
-import os
-
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-from .example_model import ExampleModel
+import example_model
 
-data = pd.read_csv("example_data.csv", header=0, names=["step", "calorie", "distance"])
 
-data = data.dropna()
+def get_training_data():
+    data = pd.read_csv(
+        "example_data.csv", header=0, names=["step", "calorie", "distance"]
+    ).dropna()
 
-x_train, y_train = data.iloc[:, 0:2].astype("float32"), data["distance"].to_frame(
-    "distance"
-).astype("float32")
-assert x_train.shape == (11, 2)
-assert y_train.shape == (11, 1)
+    x_train, y_train = data.iloc[:, 0:2].astype("float32"), data["distance"].to_frame(
+        "distance"
+    ).astype("float32")
+    assert x_train.shape == (11, 2)
+    assert y_train.shape == (11, 1)
 
-NUM_EPOCHS = 30
-BATCH_SIZE = 1
-epochs = np.arange(1, NUM_EPOCHS + 1, 1)
-losses = np.zeros([NUM_EPOCHS])
-m = ExampleModel(0.00000000003)
+    return x_train, y_train
 
-train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_ds = train_ds.batch(BATCH_SIZE)
-for i in range(NUM_EPOCHS):
-    for x, y in train_ds:
-        result = m.train(x, y)
 
-    losses[i] = result["loss"]
-    if (i + 1) % 10 == 0:
-        print(f"Finished {i+1} epochs")
-        print(f"  loss: {losses[i]:.3f}")
+def train_model(x_train, y_train):
+    NUM_EPOCHS = 30
+    BATCH_SIZE = 1
+    losses = np.zeros([NUM_EPOCHS])
+    model = example_model.ExampleModel(0.00000000003)
 
-h = m.model.predict(x_train)
-assert h.shape == (11, 1)
+    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_ds = train_ds.batch(BATCH_SIZE)
+    for i in range(NUM_EPOCHS):
+        result = {}
+        for x, y in train_ds:
+            assert model.train is not None
+            result = model.train(x, y)
+
+        losses[i] = result["loss"]
+        if (i + 1) % 10 == 0:
+            print(f"Finished {i+1} epochs")
+            print(f"  loss: {losses[i]:.3f}")
+
+    h = model.model.predict(x_train)
+    assert h.shape == (11, 1)
+
+    return model
+
+
+def save_model(model, saved_model_dir):
+    tf.saved_model.save(
+        model,
+        saved_model_dir,
+        signatures={
+            "train": model.train.get_concrete_function(),
+            "infer": model.infer.get_concrete_function(),
+            "save": model.save.get_concrete_function(),
+            "restore": model.restore.get_concrete_function(),
+        },
+    )
+
+
+def convert_saved_model(saved_model_dir):
+    # Convert the model
+    converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+    converter.target_spec.supported_ops = [
+        tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
+        tf.lite.OpsSet.SELECT_TF_OPS,  # enable TensorFlow ops.
+    ]
+
+    converter.experimental_enable_resource_variables = True
+    tflite_model = converter.convert()
+
+    return tflite_model
+
+
+def save_tflite_model(tflite_model, tflite_file):
+    with open(tflite_file, "wb") as model_file:
+        return model_file.write(tflite_model)
+
+
+def test_tflite_file(tflite_file, x_train):
+    interpreter = tf.lite.Interpreter(model_path=tflite_file)
+    interpreter.allocate_tensors()
+
+    infer = interpreter.get_signature_runner("infer")
+    h1 = infer(x=x_train).get("logits")
+    assert h1 is not None
+    print(h1[0])
+
+    train = interpreter.get_signature_runner("train")
+    result = train(
+        x=np.array([[1837, 72.332]], dtype="float32"),
+        y=np.array([[1311]], dtype="float32"),
+    )
+    print(f"Training loss: {result['loss']}.")
+
+    infer = interpreter.get_signature_runner("infer")
+    h1 = infer(x=x_train).get("logits")
+    assert h1 is not None
+    print(h1[0])
+
 
 SAVED_MODEL_DIR = "saved_model"
+TFLITE_FILE = "toy_regression.tflite"
 
-tf.saved_model.save(
-    m,
-    SAVED_MODEL_DIR,
-    signatures={
-        "train": m.train.get_concrete_function(),
-        "infer": m.infer.get_concrete_function(),
-        "save": m.save.get_concrete_function(),
-        "restore": m.restore.get_concrete_function(),
-    },
-)
 
-# Convert the model
-converter = tf.lite.TFLiteConverter.from_saved_model(SAVED_MODEL_DIR)
-converter.target_spec.supported_ops = [
-    tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
-    tf.lite.OpsSet.SELECT_TF_OPS,  # enable TensorFlow ops.
-]
+def main():
+    x_train, y_train = get_training_data()
+    model = train_model(x_train, y_train)
+    save_model(model, SAVED_MODEL_DIR)
+    tflite_model = convert_saved_model(SAVED_MODEL_DIR)
+    save_tflite_model(tflite_model, TFLITE_FILE)
+    test_tflite_file(TFLITE_FILE, x_train)
 
-converter.experimental_enable_resource_variables = True
-tflite_model = converter.convert()
 
-model_file_path = os.path.join("toy_regression.tflite")
-with open(model_file_path, "wb") as model_file:
-    model_file.write(tflite_model)
-
-interpreter = tf.lite.Interpreter(model_path="toy_regression.tflite")
-interpreter.allocate_tensors()
-
-infer = interpreter.get_signature_runner("infer")
-
-h1 = infer(x=x_train).get("logits")
-print(h1[0])
-
-train = interpreter.get_signature_runner("train")
-
-result = train(
-    x=np.array([[1837, 72.332]], dtype="float32"), y=np.array([[1311]], dtype="float32")
-)
-
-infer = interpreter.get_signature_runner("infer")
-
-h1 = infer(x=x_train).get("logits")
-print(h1[0])
+main() if __name__ == "__main__" else None
