d95063862635a50878b4725033665ac0778b054b
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-10 18:49:29 +0800

    WorkRequest for TrainWorker

--- client/fedcampus/src/main/java/org/eu/fedcampus/train/background/TrainWorker.kt
+++ client/fedcampus/src/main/java/org/eu/fedcampus/train/background/TrainWorker.kt
@@ -2,8 +2,12 @@ package org.eu.fedcampus.train.background
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.util.concurrent.TimeUnit
 
 class TrainWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
     override fun doWork() = try {
@@ -11,10 +15,20 @@ class TrainWorker(context: Context, params: WorkerParameters) : Worker(context, 
         Result.success()
     } catch (err: Throwable) {
         Log.e(TAG, err.stackTraceToString())
-        Result.failure()
+        Result.retry()
     }
 
     companion object {
         const val TAG = "TrainWorker"
     }
 }
+
+fun trainWorkRequest() = PeriodicWorkRequestBuilder<TrainWorker>(
+    12, TimeUnit.HOURS,
+    10, TimeUnit.HOURS,
+).setConstraints(realIdleConstraints())
+    .addTag(TrainWorker.TAG).build()
+
+fun realIdleConstraints() =
+    Constraints.Builder().setRequiredNetworkType(NetworkType.UNMETERED).setRequiresCharging(true)
+        .setRequiresDeviceIdle(true).build()
