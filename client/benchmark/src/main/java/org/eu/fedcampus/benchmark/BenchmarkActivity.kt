42e7d56df520497d9cfd4ffe45aa1df0ca77f6cc
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-11 10:09:46 +0800

    benchmark use same assets data as app;log into app

--- client/benchmark/src/main/java/org/eu/fedcampus/benchmark/BenchmarkActivity.kt
+++ client/benchmark/src/main/java/org/eu/fedcampus/benchmark/BenchmarkActivity.kt
@@ -1,8 +1,12 @@
 package org.eu.fedcampus.benchmark
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import org.eu.fedcampus.benchmark.databinding.ActivityBenchmarkBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class BenchmarkActivity : AppCompatActivity() {
     lateinit var binding: ActivityBenchmarkBinding
@@ -10,5 +14,21 @@ class BenchmarkActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityBenchmarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        appendLog("Initialized.")
+    }
+
+    private fun appendLog(log: String) {
+        val dateFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+        val time = dateFormat.format(Date())
+        val text = "$time: $log\n"
+        Log.i(TAG, "appendLog: $text")
+        runOnUiThread {
+            binding.tvHello.append(text)
+        }
+    }
+
+    companion object {
+        const val TAG = "BenchmarkActivity"
     }
 }
