9bc16b2d271ee281aae2562e13b338ab53437d59
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-11 10:42:25 +0800

    handle button click

--- client/benchmark/src/main/java/org/eu/fedcampus/benchmark/BenchmarkActivity.kt
+++ client/benchmark/src/main/java/org/eu/fedcampus/benchmark/BenchmarkActivity.kt
@@ -1,7 +1,13 @@
 package org.eu.fedcampus.benchmark
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.webkit.URLUtil
 import androidx.appcompat.app.AppCompatActivity
 import org.eu.fedcampus.benchmark.databinding.ActivityBenchmarkBinding
 import java.text.SimpleDateFormat
@@ -15,7 +21,27 @@ class BenchmarkActivity : AppCompatActivity() {
         binding = ActivityBenchmarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        enterClickSub()
+
         appendLog("Initialized.")
+    }
+
+    private fun submit() {
+        hideKeyboard()
+        val uri = binding.etUri.text.toString()
+        val url = "http://$uri:8000"
+        if (!isUrl(url)) {
+            appendLog("$url is invalid, please input a valid URI!")
+            return
+        }
+    }
+
+    private fun enterClickSub() = binding.etUri.setOnEditorActionListener { _, actionId, _ ->
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            binding.btSub.performClick()
+            return@setOnEditorActionListener true
+        }
+        false
     }
 
     private fun appendLog(log: String) {
@@ -28,7 +54,19 @@ class BenchmarkActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("ServiceCast")
+    private fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        var view = currentFocus
+        if (view == null) {
+            view = View(this)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
     companion object {
         const val TAG = "BenchmarkActivity"
     }
 }
+
+fun isUrl(url: String) = URLUtil.isValidUrl(url) && Patterns.WEB_URL.matcher(url).matches()
