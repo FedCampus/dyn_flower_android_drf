27077589cf44bc06f78093d28dcc058873a37efe
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-19 13:52:06 +0800

    extract upload fn

--- fed_kit.py
+++ fed_kit.py
@@ -2,16 +2,24 @@
 """CLI App for FedKit backend operations."""
 import requests
 
+DEFAULT_URL = "http://localhost:8000/"
+
+
+def upload(base: str, file: str, name: str, layers_sizes: list[int], data_type: str):
+    url = base + "/train/upload"
+    files = {"file": open(file, "rb")}
+    data = {
+        "name": name,
+        "layers_sizes": layers_sizes,
+        "data_type": data_type,
+    }
+    return requests.post(url, data=data, files=files)
+
 
 def main():
-    url = "http://localhost:8000/train/upload"  # Replace with your Django server URL
-    files = {"file": open("test.txt", "rb")}
-    data = {
-        "name": "test_model",
-        "layers_sizes": [100, 200, 300],
-        "data_type": "test_type",
-    }
-    response = requests.post(url, data=data, files=files)
+    response = upload(
+        DEFAULT_URL, "test.txt", "test_model", [100, 200, 300], "test_type"
+    )
     print(response)
     print(response.text)
 
