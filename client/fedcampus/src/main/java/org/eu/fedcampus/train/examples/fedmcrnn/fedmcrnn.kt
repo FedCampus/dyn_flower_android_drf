51943fa4f916c448ec91054d798d0f954d1680dd
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-19 10:49:22 +0800

    basic helpers for FedMCRNN

---
+++ client/fedcampus/src/main/java/org/eu/fedcampus/train/examples/fedmcrnn/fedmcrnn.kt
@@ -0,0 +1,17 @@
+package org.eu.fedcampus.train.examples.fedmcrnn
+
+import org.eu.fedcampus.train.SampleSpec
+import org.eu.fedcampus.train.helpers.negativeLogLikelihoodLoss
+import org.eu.fedcampus.train.helpers.placeholderAccuracy
+
+fun sampleSpec() = SampleSpec<Float2DArray, FloatArray>(
+    { it.toTypedArray() },
+    { it.toTypedArray() },
+    { Array(it) { FloatArray(1) } },
+    ::negativeLogLikelihoodLoss,
+    ::placeholderAccuracy,
+)
+
+const val DATA_TYPE = "FedMCRNN_6x7"
+
+typealias Float2DArray = Array<FloatArray>
