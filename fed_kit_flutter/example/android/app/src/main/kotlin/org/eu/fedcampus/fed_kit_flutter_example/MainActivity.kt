1f67040436765da202f3ac2fb1de92f6fdc81c9b
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-24 12:51:49 +0800

    format FedKitFlutterPlugin code

--- fed_kit_flutter/example/android/app/src/main/kotlin/org/eu/fedcampus/fed_kit_flutter_example/MainActivity.kt
+++ fed_kit_flutter/example/android/app/src/main/kotlin/org/eu/fedcampus/fed_kit_flutter_example/MainActivity.kt
@@ -2,5 +2,5 @@ package org.eu.fedcampus.fed_kit_flutter_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
