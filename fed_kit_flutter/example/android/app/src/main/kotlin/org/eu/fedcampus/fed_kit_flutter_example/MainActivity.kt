a9e0d80eb6ece80c8df17bc8b1d235dd9f19cfe8
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-26 10:36:03 +0800

    rm fed_kit_flutter plugin

--- fed_kit_flutter/example/android/app/src/main/kotlin/org/eu/fedcampus/fed_kit_flutter_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package org.eu.fedcampus.fed_kit_flutter_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
--- fed_kit_flutter/lib/fed_kit_flutter.dart
+++
@@ -1,7 +0,0 @@
-import 'fed_kit_flutter_platform_interface.dart';
-
-class FedKitFlutter {
-  Future<String?> getPlatformVersion() {
-    return FedKitFlutterPlatform.instance.getPlatformVersion();
-  }
-}
